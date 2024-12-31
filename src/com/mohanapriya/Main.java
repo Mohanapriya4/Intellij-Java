c1c0eec1765e2537fff64521c4002c71a0c6dd74
Mohanapriya4 <73380833+Mohanapriya4@users.noreply.github.com>
2024-12-31 19:41:22 +0530

    Java-loops

---
+++ src/com/mohanapriya/Main.java
@@ -0,0 +1,15 @@
+package com.mohanapriya;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My first program in intellij");
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Pls enter your name: ");
+//        String a = scan.nextLine();
+//        System.out.println(a);
+        for(int num=0;num<=5;num+=1){
+            System.out.println(num);
+            System.out.println("done");
+        }
+    }
+}
